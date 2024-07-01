e24d289376076f707acc26fc11e767c956c86bee
Yahia Kamal <93154332+motakosay@users.noreply.github.com>
2024-07-02 00:14:42 +0300

    Update utils.py
    just read the code

--- utils.py
+++ utils.py
@@ -252,7 +252,8 @@ def img2patches(img, m_grid, s, to_tensor=True):
     if to_tensor:
         img_batch = torch.tensor(img_batch)
 
-    return img_batch
+    #return img_batch
+    return patch
 
 
 
