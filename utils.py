162dc64390a3596beb9e8e19b7c0dce9b69a885e
Yahia Kamal <93154332+motakosay@users.noreply.github.com>
2024-07-02 01:22:08 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -245,6 +245,7 @@ def img2patches(img, m_grid, s, to_tensor=True):
     img_batch = np.zeros([m_grid ** 2, 3, s, s], np.float32)
     for y_id in range(m_grid):
         for x_id in range(m_grid):
+            print(x_id)
             patch = img[y_id * s:y_id * s + s,
                     x_id * s:x_id * s + s, :].transpose([2, 0, 1])
             img_batch[y_id * m_grid + x_id, :, :, :] = patch
