9eb757926beaf189a8ea5d726a3eff20f80c96ef
Yahia Kamal <93154332+motakosay@users.noreply.github.com>
2024-07-02 20:08:43 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -153,13 +153,12 @@ def set_requires_grad(nets, requires_grad=False):
     if not isinstance(nets, list):
         nets = [nets]
     for net in nets:
-        print("gooood", net)
-        """
         if net is not None:
             for param in net.parameters():
+                print(param)
+                """
                 param.requires_grad = requires_grad
-        """
-
+                """
 
 
 def make_numpy_grid(tensor_data):
