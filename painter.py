475e5bd7a82d7c7c73e8c47e6af11281f6c8599a
Yahia Kamal <93154332+motakosay@users.noreply.github.com>
2024-07-02 14:52:49 +0300

    Update painter.py

--- painter.py
+++ painter.py
@@ -192,8 +192,6 @@ class PainterBase():
 
     def initialize_params(self):
 
-        print(self.m_grid)
-        """
         self.x_ctt = np.random.rand(
             self.m_grid*self.m_grid, self.m_strokes_per_block,
             self.rderr.d_shape).astype(np.float32)
@@ -208,7 +206,6 @@ class PainterBase():
             self.m_grid*self.m_grid, self.m_strokes_per_block,
             self.rderr.d_alpha).astype(np.float32)
         self.x_alpha = torch.tensor(self.x_alpha).to(device)
-        """
 
     def stroke_sampler(self, anchor_id):
 
