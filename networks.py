749e6265b6cf2d7b3ff4d651d7a2a67f91699f23
Yahia Kamal <93154332+motakosay@users.noreply.github.com>
2024-07-03 12:58:45 +0300

    Update networks.py

--- networks.py
+++ networks.py
@@ -58,8 +58,8 @@ def init_weights(net, init_type='normal', init_gain=0.02):
         classname = m.__class__.__name__
         if hasattr(m, 'weight') and (classname.find('Conv') != -1 or classname.find('Linear') != -1):
             if init_type == 'normal':
-                print(m.weight.data)
-                init.normal_(m.weight.data, 0.0, init_gain)
+                result = init.normal_(m.weight.data, 0.0, init_gain)
+                print(result)
             elif init_type == 'xavier':
                 init.xavier_normal_(m.weight.data, gain=init_gain)
             elif init_type == 'kaiming':
