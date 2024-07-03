53f7d0bd509f7cd9c170ec87b23c1f681b3951b1
Yahia Kamal <93154332+motakosay@users.noreply.github.com>
2024-07-03 19:29:02 +0300

    Update networks.py

--- networks.py
+++ networks.py
@@ -56,7 +56,7 @@ def init_weights(net, init_type='normal', init_gain=0.02):
     """
     def init_func(m):  # define the initialization function
         classname = m.__class__.__name__
-        if hasattr(m, 'weight'):
+        if not hasattr(m, 'weight'):
             print(classname)
         """
         classname = m.__class__.__name__
