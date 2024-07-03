0447dcf29197f5fddaa79fb4451b9b654346e8fb
Yahia Kamal <93154332+motakosay@users.noreply.github.com>
2024-07-03 13:19:35 +0300

    Update networks.py

--- networks.py
+++ networks.py
@@ -56,7 +56,6 @@ def init_weights(net, init_type='normal', init_gain=0.02):
     """
     def init_func(m):  # define the initialization function
         classname = m.__class__.__name__
-        print(classname)
         if hasattr(m, 'weight') and (classname.find('Conv') != -1 or classname.find('Linear') != -1):
             if init_type == 'normal':
                 init.normal_(m.weight.data, 0.0, init_gain)
