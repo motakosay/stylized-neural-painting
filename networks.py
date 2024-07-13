5a5ee5225be149e4315d83c1c8f423a5951eae1f
Yahia Kamal <93154332+motakosay@users.noreply.github.com>
2024-07-13 23:28:33 +0300

    Update networks.py

--- networks.py
+++ networks.py
@@ -289,6 +289,7 @@ class ZouFCNFusion(nn.Module):
     def forward(self, x):
         x_shape = x[:, 0:self.rdrr.d_shape, :, :]
         x_alpha = x[:, [-1], :, :]
+        print(x_alpha)
         if self.rdrr.renderer in ['oilpaintbrush', 'airbrush']:
             x_alpha = torch.tensor(1.0).to(device)
 
