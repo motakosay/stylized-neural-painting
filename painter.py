04ea6ce89c29a611f5bad198b82eb08dedd756a3
Yahia Kamal <93154332+motakosay@users.noreply.github.com>
2024-07-18 00:08:22 +0300

    Update painter.py

--- painter.py
+++ painter.py
@@ -252,12 +252,13 @@ class PainterBase():
 
         v = torch.reshape(self.x[:, 0:self.anchor_id+1, :],
                           [self.m_grid*self.m_grid*(self.anchor_id+1), -1, 1, 1])
-        self.G_pred_foregrounds, self.G_pred_alphas = self.net_G(v)
+        #self.G_pred_foregrounds, self.G_pred_alphas = self.net_G(v)
+        self.G_pred_alphas = self.net_G(v)
 
-        self.G_pred_foregrounds = morphology.Dilation2d(m=1)(self.G_pred_foregrounds)
+        #self.G_pred_foregrounds = morphology.Dilation2d(m=1)(self.G_pred_foregrounds)
         self.G_pred_alphas = morphology.Erosion2d(m=1)(self.G_pred_alphas)
 
-        self.G_pred_foregrounds = torch.reshape(
+        #self.G_pred_foregrounds = torch.reshape(
             self.G_pred_foregrounds, [self.m_grid*self.m_grid, self.anchor_id+1, 3,
                                       self.net_G.out_size, self.net_G.out_size])
         self.G_pred_alphas = torch.reshape(
@@ -265,7 +266,7 @@ class PainterBase():
                                  self.net_G.out_size, self.net_G.out_size])
 
         for i in range(self.anchor_id+1):
-            G_pred_foreground = self.G_pred_foregrounds[:, i]
+            #G_pred_foreground = self.G_pred_foregrounds[:, i]
             G_pred_alpha = self.G_pred_alphas[:, i]
             self.G_pred_canvas = G_pred_foreground * G_pred_alpha \
                                  + self.G_pred_canvas * (1 - G_pred_alpha)
