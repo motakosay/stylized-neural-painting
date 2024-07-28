1d76630a97c01b5b0a71aac785ae13b80e5410fb
Yahia Kamal <93154332+motakosay@users.noreply.github.com>
2024-07-28 04:35:05 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -245,13 +245,14 @@ def img2patches(img, m_grid, s, to_tensor=True):
     img_batch = np.zeros([m_grid ** 2, 3, s, s], np.float32)
     for y_id in range(m_grid):
         for x_id in range(m_grid):
+            patch = img.transpose([2, 0, 1]
+            print("patchWithoutKabala", patch)
+                                  
             patch = img[y_id * s:y_id * s + s,
                     x_id * s:x_id * s + s, :].transpose([2, 0, 1])
-            print("shapeOfPatch", patch.shape)
-            print("patch_only", patch)
-            print("before", img_batch.shape)
+            print("patchWithKabala", patch)
+            
             img_batch[y_id * m_grid + x_id, :, :, :] = patch
-            print("after", img_batch.shape)
 
     if to_tensor:
         img_batch = torch.tensor(img_batch)
