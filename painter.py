204dfa65403c703aa0a1446dc7ee83d73be3e95b
Yahia Kamal <93154332+motakosay@users.noreply.github.com>
2024-08-03 23:14:09 +0300

    Update painter.py

--- painter.py
+++ painter.py
@@ -69,7 +69,6 @@ class PainterBase():
                                 map_location=None if torch.cuda.is_available() else device)
 
             # update net_G states
-            print("huangnet.conv2.weight" , checkpoint['model_G_state_dict']['huangnet.conv3.weight'].shape)
             self.net_G.load_state_dict(checkpoint['model_G_state_dict'])
             self.net_G.to(device)
             self.net_G.eval()
