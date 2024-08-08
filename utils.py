ae9be2fbedadcbaa7523b78f4c2106d8b1bb706d
Yahia Kamal <93154332+motakosay@users.noreply.github.com>
2024-08-08 12:10:39 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -287,6 +287,7 @@ def create_transformed_brush(brush, canvas_w, canvas_h,
     brush_alpha = np.stack([brush, brush, brush], axis=-1)
     brush_alpha = (brush_alpha > 0).astype(np.float32)
     brush_alpha = (brush_alpha*255).astype(np.uint8)
+    print("brush_alpha", brush_alpha)
     colormap = np.zeros([brush.shape[0], brush.shape[1], 3], np.float32)
     for ii in range(brush.shape[0]):
         t = ii / brush.shape[0]
