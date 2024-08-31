c6fe8cf07f0d10b4511d422e238b6bda27002c36
Yahia Kamal <93154332+motakosay@users.noreply.github.com>
2024-08-31 16:20:51 +0300

    Update loss.py

--- loss.py
+++ loss.py
@@ -27,10 +27,10 @@ class PixelLoss(nn.Module):
         print("canvas_v", canvas_v.shape)
         print("batch_v", batch_v.shape)
             
-        plt.imshow(canvas_v), plt.title('canvas')
+        plt.imshow(canvas_v.detach().numpy()), plt.title('canvas')
         plt.show()
             
-        plt.imshow(batch_v), plt.title('gen_batch')
+        plt.imshow(batch_v.detach().numpy()), plt.title('gen_batch')
         plt.show()
         if ignore_color:
             canvas = torch.mean(canvas, dim=1)
